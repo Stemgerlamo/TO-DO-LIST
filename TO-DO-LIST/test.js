c4fca619a814e8b7b9e3abe928bfe40297a1ac63
Gervais Ngabo <117227637+Stemgerlamo@users.noreply.github.com>
2024-12-14 00:37:24 +0200

    Update test.js

--- TO-DO-LIST/test.js
+++ TO-DO-LIST/test.js
@@ -1,8 +1,6 @@
 //selecting list element
 const list=document.getElementById("toDoList");
-const cont=document.getElementById("container");
 const value=document.getElementById("input");
-const button=document.getElementById("button");
 
 //adding events
 function add(){
